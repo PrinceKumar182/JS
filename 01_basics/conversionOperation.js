1600fabe338191ae5d85573f9fb61ad068e55834
Prince Kumar <108454597+PrinceKumar182@users.noreply.github.com>
2024-01-26 22:50:19 +0000

    converOperation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,33 @@
+let score="33"
+
+console.log(typeof score)
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+
+//"33"=>33
+//"33abc"=>Nan, but it will type as number
+// true=>1
+let isLoggedIn=5
+let booleanIsLoggedin=Boolean(isLoggedIn)
+console.log(booleanIsLoggedin)
+
+// 1=>true; 0=>false; ""=>false and any other thing true
+
+let someNumber=4
+let StringNumber=String(someNumber)
+console.log(someNumber)
+console.log(typeof StringNumber)
+
+
+// *******************Operations************************
+
+let value=3
+let negValue=-value
+
+//** for power and % for modulo
+
+console.log("1"+2) //12
+console.log(1+"2") //12
+console.log("1"+2+2)  // 122
+console.log(2+2+"1") //41
