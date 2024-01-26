2c68dbf76d2cc2535729bd16616447e2cce493d9
Prince Kumar <108454597+PrinceKumar182@users.noreply.github.com>
2024-01-26 22:24:06 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+// alert("hello")    we are using node.js not browser
+
+console.log(3+3)
+console.log(4)
+
+console.log(3+3); console.log(4)
+
+//  https://tc39.es/   for documentation
+let name="Prince"
+let age=20
+let isLoggeIn=true
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//null=>standalone value
+//undefined =>
+// symbol=> unique
+
+// object
+
+console.log(typeof "Prince")
+console.log(typeof null)
+
